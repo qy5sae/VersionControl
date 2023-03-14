be4412b65c7d470b73ba8593cd9950e8a94957e3
Nóra Vass <nora.vass2@stud.uni-corvinus.hu>
2023-03-14 09:03:22 +0100

    Excel munkafuzet letrehozasa és megnyitása

--- excel04/excel04/Form1.cs
+++ excel04/excel04/Form1.cs
@@ -16,14 +16,43 @@ namespace excel04
     {
         List<Flat> Flats;
         RealEstateEntities context = new RealEstateEntities();
+        Excel.Application xlApp;
+        Excel.Workbook xlWb;
+        Excel.Worksheet xlSheet;
         public Form1()
         {            
             InitializeComponent();
             LoadData();
+            
         }
         void LoadData()
         {
             Flats = context.Flats.ToList();
+        }
+        
+        void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+                xlWb = xlApp.Workbooks.Add(Missing.Value);
+                xlSheet = xlWb.ActiveSheet;
+
+                //CreateTable()
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                xlWb.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWb = null;
+                xlApp = null;
+            }
         }
     }
 }
