f39d176a58c129e83c464271895a47ad8ddc8b6d
Nóra Vass <nora.vass2@stud.uni-corvinus.hu>
2023-03-28 08:57:44 +0200

    arfolyam lista

--- MnbCurrencyReader2/MnbCurrencyReader2/Form1.cs
+++ MnbCurrencyReader2/MnbCurrencyReader2/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MnbCurrencyReader2.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,27 @@ namespace MnbCurrencyReader2
         public Form1()
         {
             InitializeComponent();
+            fuggveny();
+        }
+
+        private static void fuggveny()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            // Ebben az esetben a "var" a GetExchangeRates visszatérési értékéből kapja a típusát.
+            // Ezért a response változó valójában GetExchangeRatesResponseBody típusú.
+            var response = mnbService.GetExchangeRates(request);
+
+            // Ebben az esetben a "var" a GetExchangeRatesResult property alapján kapja a típusát.
+            // Ezért a result változó valójában string típusú.
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
