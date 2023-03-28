60d3465f728318df92cdbd1a32827fe2b88eec24
Nóra Vass <nora.vass2@stud.uni-corvinus.hu>
2023-03-28 08:36:00 +0200

    Initial commit

---
+++ MnbCurrencyReader/MnbCurrencyReader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MnbCurrencyReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
